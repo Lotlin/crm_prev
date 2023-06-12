03b3db01aabf6b56d0efe40b4c502fd4d1962ec2
Lotlin <lotlin22@gmail.com>
2023-06-12 22:23:07 +0200

    Make HW l_3 task_2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const addGood = document.querySelector('.add-good');
 addGood.classList.add('add-good--visible');
 const addGoodVision = document.querySelector('.add-good--visible');
@@ -34,3 +33,84 @@ goodErrorVision.style.setProperty('display', 'flex');
 
 const goodErrorHeader = changeGood.querySelector('.add-good__error-title');
 const goodErrorCloseButton = changeGood.querySelector('.add-good__error-close-button');
+
+const testArr = [
+  {
+    'id': 246016548,
+    'title': 'Навигационная система Soundmax',
+    'category': 'Техника для дома',
+    'units': 'шт',
+    'amount': 5,
+    'price': 300,
+    'total': 1500,
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+      'big': 'img/smrtxiaomi11t-b.jpg',
+    },
+  },
+  {
+    'id': 215796548,
+    'title': 'Радиоуправляемый автомобиль Cheetan',
+    'category': `Внедорожник на дистанционном управлении. 
+      Скорость 25км/ч. Возраст 7 - 14 лет`,
+    'units': 'шт',
+    'amount': 3,
+    'price': 100,
+    'total': 300,
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+      'big': 'img/smrtxiaomi11t-b.jpg',
+    },
+  },
+  {
+    'id': 215796548,
+    'title': 'Витая пара PROConnect 01-0043-3-25',
+    'category': `Всего лишь один шаг сделает ваш телевизор умным, 
+      Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, 
+      сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D`,
+    'units': 'шт',
+    'amount': 70,
+    'price': 10,
+    'total': 700,
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+    },
+  },
+];
+
+const parsingNestedObject = (obj) => {
+  const valArr = [];
+  const nestedValues = Object.values(obj);
+  for (const val of nestedValues) {
+    valArr.push(val);
+  }
+
+  return valArr;
+};
+
+const createRow = (obj) => {
+  const elem = document.createElement('tr');
+  const values = Object.values(obj);
+
+  for (let value of values) {
+    if (typeof value === 'object') {
+      value = parsingNestedObject(value);
+    }
+
+    elem.insertAdjacentHTML('beforeend', `<td>${value}</td>`);
+  }
+
+  return elem;
+};
+
+const renderGoods = (arr, table) => {
+  arr.map((item) => {
+    const newRow = createRow(item);
+    table.appendChild(newRow);
+  });
+
+  return true;
+};
+
+const table = document.querySelector('.goods__table');
+renderGoods(testArr, table);
